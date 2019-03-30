04d65896237f22f094db163ee0b07d0c976a191e
Andrei <mobilelookfree@gmail.com>
2019-03-30 14:02:14 +0300

    pathetic request fix

--- deanery-ivbo-02-15-1/src/services/request.js
+++ deanery-ivbo-02-15-1/src/services/request.js
@@ -1,7 +1,32 @@
+let myOptions = {
+  mode: 'no-cors'
+};
+
+//fetch("http://localhost:8080/students", myOptions).then(res => res.json);
+
+/* 
+fetch('http://localhost:8080/students', myOptions).then((res) => {
+      res.status === 200 ? res.json().then((res) => {console.log(res[0])}) : console.log('fuck');
+    });
+*/
+
+/*
+fetch('http://localhost:8080/students', myOptions).then((res) => {
+        res.json().then((res) => {
+            console.log('Output: ', res[0]);
+          })
+          .catch((err) => console.error(err));
+    });
+*/
+
 const Request = {
-    getStudents() {
-        return fetch("http://localhost:8080/students").then(res => res.json);
-    }
+  getStudents() {
+    return fetch('http://localhost:8080/students', myOptions).then((res) => {
+      res.status === 200
+        ? res.json().then((res) => console.log('Output: ', res[0]))
+        : console.log('fuck');
+    });
+  }
 };
 
 export default Request;
